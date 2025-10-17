908c15eb5acecd81447d2c98720b48cb9ae16550
AGRATEROL <73317322+A-Graterol@users.noreply.github.com>
2025-10-17 10:25:34 -0400

    Add service worker with install and fetch events
    Implement service worker for caching assets

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("cartas-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "style.css",
+        "script.js",
+        "manifest.json",
+        "icon-192.png",
+        "icon-512.png"
+      ]);
+    })
+  );
+});
 
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
